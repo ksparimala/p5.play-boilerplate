f3670754e9b5de7a5b13a3e6f78102f48b7112b0
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-01-30 15:36:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,42 @@
+//physics engine
+//1.world    2.engine   3.body   
+//name spacing 
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+
+//declaring my variable
+var engine,world,object;
 function setup() {
   createCanvas(800,400);
-  //create two rectangles  a=fixed rectangle    b=moving rectangle
-  a=createSprite(200,200,50,50); //width of a=50  ,a.width/2=25
-  b=createSprite(400,200,50, 50);//width of b=50  ,b.width/2=25   25+25=50=a.width/2+b.width/2
-  a.shapeColor="green";
-  b.shapeColor="green";
+
+  //create the engine
+  engine=Engine.create();
+  world=engine.world;
+
+  var options=
+ {
+  isStatic:true
+ }
+  object=Bodies.rectangle(200,200,50,50,options);
+
+  //adding world and body
+  World.add(world,object);
+  
+
+  console.log(object);
+  console.log(object.type);
+  console.log(object.position.x);
+  console.log(object.position.y);
+  
 }
 
 function draw() {
-  background("yellow"); 
-  b.x=mouseX;
-  b.y=mouseY; 
-  //distance between the rectangle x position
-  console.log(b.x-a.x);
-  if(b.x-a.x<a.width/2+b.width/2 && a.x-b.x<a.width/2+b.width/2)
-  {
-    a.shapeColor="red";
-    b.shapeColor="red";
-  }
-  else
-  {
-    a.shapeColor="green";
-    b.shapeColor="green";
-  }
-  drawSprites();
+  background("black");  
+  Engine.update(engine);
+  //draw a rectangle 
+  rectMode(CENTER);
+  rect(400,200,50,50);
+
+  rect(object.position.x,object.position.y,50,50)
 }
