773ef6b2b3a516f89cf9c0761174113e3bb048e7
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-03-01 20:29:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,33 @@
-//physics engine
-//1.world    2.engine   3.body   
-//name spacing 
-const Engine=Matter.Engine
-const World=Matter.World
-const Bodies=Matter.Bodies
-
-//declaring my variable
-var engine,world,object;
 function setup() {
   createCanvas(800,400);
 
-  //create the engine
-  engine=Engine.create();
-  world=engine.world;
-
-  var options=
- {
-  isStatic:true
- }
-  object=Bodies.rectangle(200,200,50,50,options);
-
-  //adding world and body
-  World.add(world,object);
-  
-
-  console.log(object);
-  console.log(object.type);
-  console.log(object.position.x);
-  console.log(object.position.y);
-  
-}
-
+  movingRect=createSprite(400, 200, 100, 50);  //movingrect.width/2=100/2=50
+  movingRect.shapeColor="green";
+  fixedRect=createSprite(200,200,50,80)      //fixedRect.width/2=50/2=25
+  fixedRect.shapeColor="green";              //movingRect.width/2+fixedRect.width/2=50+25=75
+}                                            //movingRect.x-fixedRect.x=50
+//movingRect.x-fixedRect.x=movingRect.width/2+fixedRect.width/2
 function draw() {
-  background("black");  
-  Engine.update(engine);
-  //draw a rectangle 
-  rectMode(CENTER);
-  rect(400,200,50,50);
+  background(255,255,255);  
+  console.log(movingRect.x-fixedRect.x);
+  //if it is == it will work when it touch only
+  //2.if it positive it will work rightside
+  //3.if it is negative it will work  for leftside
+  //comblined  2 and 3 point
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
+     fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
+     movingRect.y-fixedRect.y<movingRect.width/2+fixedRect.width/2&&
+     fixedRect.y-movingRect.y<movingRect.width/2+fixedRect.width/2)
+  {
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
 
-  rect(object.position.x,object.position.y,50,50)
+  movingRect.x=mouseX;
+  movingRect.y=mouseY;
+  drawSprites();
 }
