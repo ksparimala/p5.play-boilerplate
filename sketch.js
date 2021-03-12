0f072132ddaf2c98498f29f8d47e25ecd9a17c35
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-03-12 12:50:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,30 @@
 function setup() {
   createCanvas(800,400);
-
-  movingRect=createSprite(400, 200, 100, 50);  //movingrect.width/2=100/2=50
+  movingRect=createSprite(400, 200, 80, 80); //movingrect.width/2=80/2=40
   movingRect.shapeColor="green";
-  fixedRect=createSprite(200,200,50,80)      //fixedRect.width/2=50/2=25
-  fixedRect.shapeColor="green";              //movingRect.width/2+fixedRect.width/2=50+25=75
-}                                            //movingRect.x-fixedRect.x=50
-//movingRect.x-fixedRect.x=movingRect.width/2+fixedRect.width/2
+  fixedRect=createSprite(200,200,80,80);//fixedrect.width/2=80/2=40
+  fixedRect.shapeColor="green";//movingrect.width/2+fixedrect.width/2=40+40=80=movingRect.x-fixedRect.x
+}
+
 function draw() {
-  background(255,255,255);  
-  console.log(movingRect.x-fixedRect.x);
-  //if it is == it will work when it touch only
-  //2.if it positive it will work rightside
-  //3.if it is negative it will work  for leftside
-  //comblined  2 and 3 point
-  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
-     fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
-     movingRect.y-fixedRect.y<movingRect.width/2+fixedRect.width/2&&
-     fixedRect.y-movingRect.y<movingRect.width/2+fixedRect.width/2)
+  background("yellow");  
+  //movingRect.x-fixedRect.x=50
+  console.log(fixedRect.x-movingRect.x);
+
+  //the distance between 2 rectangle x  positions is eqal to sum of the 
+  //half of the width of the rectangles
+  //80 postive  making -80
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&& 
+     fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2)
   {
     movingRect.shapeColor="red";
     fixedRect.shapeColor="red";
   }
-  else{
+  else
+  {
     movingRect.shapeColor="green";
     fixedRect.shapeColor="green";
   }
-
   movingRect.x=mouseX;
   movingRect.y=mouseY;
   drawSprites();
