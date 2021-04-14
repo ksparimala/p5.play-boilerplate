373f1d75dbe56203bb25b70e2852c016919cf24c
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-04-14 15:40:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,20 @@
+var movingRect,fixedRect
 function setup() {
-  createCanvas(800,400);
-  movingRect=createSprite(400, 200, 80, 80); //movingrect.width/2=80/2=40
+  createCanvas(800,400); 
+  movingRect=createSprite(400,200,50,50); //movingrect.width/2=60/2=30 
   movingRect.shapeColor="green";
-  fixedRect=createSprite(200,200,80,80);//fixedrect.width/2=80/2=40
-  fixedRect.shapeColor="green";//movingrect.width/2+fixedrect.width/2=40+40=80=movingRect.x-fixedRect.x
+  fixedRect=createSprite(600,200,50,50);//fixedrect.width/2=60/2=30
+  fixedRect.shapeColor="green";      //movingrect.width/2+fixedrect.width/2=30+30=60
 }
 
 function draw() {
-  background("yellow");  
-  //movingRect.x-fixedRect.x=50
-  console.log(fixedRect.x-movingRect.x);
-
-  //the distance between 2 rectangle x  positions is eqal to sum of the 
-  //half of the width of the rectangles
-  //80 postive  making -80
-  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&& 
-     fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2)
+  background("lightgray"); 
+  console.log("first"+(fixedRect.x-movingRect.x));
+  console.log("second"+(movingRect.width/2+fixedRect.width/2));
+  //the distance of the x position of the sprites(fixedRect.x-movingRect.x=50) is 
+  //equal to the half of the width of the both sprites(movingrect.width/2+fixedrect.width/2=50)
+  if(fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
+    movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2)
   {
     movingRect.shapeColor="red";
     fixedRect.shapeColor="red";
@@ -25,7 +24,7 @@ function draw() {
     movingRect.shapeColor="green";
     fixedRect.shapeColor="green";
   }
-  movingRect.x=mouseX;
-  movingRect.y=mouseY;
+  fixedRect.x=mouseX;
+  fixedRect.y=mouseY;
   drawSprites();
 }
