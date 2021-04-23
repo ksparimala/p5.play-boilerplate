a01cc557dc5bdfb64fa615b320b74732cf5c47e6
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-04-23 12:40:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,35 @@
-var movingRect,fixedRect
+//name spacing  
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var object;
+
 function setup() {
-  createCanvas(800,400); 
-  movingRect=createSprite(400,200,50,50); //movingrect.width/2=60/2=30 
-  movingRect.shapeColor="green";
-  fixedRect=createSprite(600,200,50,50);//fixedrect.width/2=60/2=30
-  fixedRect.shapeColor="green";      //movingrect.width/2+fixedrect.width/2=30+30=60
-}
+  createCanvas(800,400);
+  engine=Engine.create();
+  world=engine.world;
+  
+  var options={
+    isStatic:true
+  }
+
+  object=Bodies.rectangle(200,100,50,50,options)
+  World.add(world,object)
+  
+  console.log(object);
+  console.log(object.position.x);
+  console.log(object.position.y);
+
+
+ }
 
 function draw() {
-  background("lightgray"); 
-  console.log("first"+(fixedRect.x-movingRect.x));
-  console.log("second"+(movingRect.width/2+fixedRect.width/2));
-  //the distance of the x position of the sprites(fixedRect.x-movingRect.x=50) is 
-  //equal to the half of the width of the both sprites(movingrect.width/2+fixedrect.width/2=50)
-  if(fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2&&
-    movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2)
-  {
-    movingRect.shapeColor="red";
-    fixedRect.shapeColor="red";
-  }
-  else
-  {
-    movingRect.shapeColor="green";
-    fixedRect.shapeColor="green";
-  }
-  fixedRect.x=mouseX;
-  fixedRect.y=mouseY;
-  drawSprites();
+  background(0);  
+  Engine.update(engine);
+  rectMode("center")
+  rect(400, 200, 50, 50)
+  rect(object.position.x,object.position.y,100,100);
+ 
 }
