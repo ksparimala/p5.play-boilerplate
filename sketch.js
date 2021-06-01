b56b99c0f75e6b6957c144ec855a2f79220c2da9
ksparimala <72539701+ksparimala@users.noreply.github.com>
2021-06-01 12:39:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-//we will create our own algorithm for istouching that is if one rectangle is touchiing 
-//the other rectangle then both the rectangle color should change
-//algorithm is step by step process
-//create one fixedrect and movingrect
+//functions and arguments
+//user defined functions-DRY
+var car,wall,fixedRect,movingRect;
 function setup() {
   createCanvas(800,400);
   fixedRect=createSprite(400, 200,50, 100);   //frw/2=80/2=40
   fixedRect.shapeColor="red";
   movingRect=createSprite(600, 200, 100, 50);   //movingrectwidth/2=80/2=40
   movingRect.shapeColor="red";                 //fixedrectwidth/2+movingrectwidth/2=40+40=80
+
+  car=createSprite(200,100,100,50)
+  car.shapeColor="pink";
+  car.velocityX=2;
+
+  wall=createSprite(600,100,50,100)
+  wall.shapeColor="pink";
+
 }
 
 function draw() {
   background("yellow"); 
-  console.log(movingRect.x-fixedRect.x);
-  movingRect.x=mouseX;  //5-3=2      3-5=-2
+  movingRect.x=mouseX;  
   movingRect.y=mouseY;
-  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2&&
-     fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2)
+  //calling the function-friend1
+  if(isTouching(movingRect,fixedRect))
   {
     fixedRect.shapeColor="green";
-    movingRect.shapeColor="green"; 
+   movingRect.shapeColor="green";
   }
   else{
     fixedRect.shapeColor="red";
     movingRect.shapeColor="red";
+  }
+  //friend-2
+  if(isTouching(car,wall))
+  {
+   car.velocityX=0;
+  }
+  else
+  {
+    car.shapeColor="pink";
+    wall.shapeColor="pink";
   }
   drawSprites();
 }
